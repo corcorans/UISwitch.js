fbe4535925abf4ed5f9afa3de72d4d0f7afc2c6a
Daniel Imhoff <dwieeb@gmail.com>
2014-09-02 18:24:35 -0500

    Adding `umd` task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+var wrap = require('gulp-wrap-umd');
 
 gulp.task('default', function() {
     // Default task
+});
+
+gulp.task('umd', function() {
+    gulp.src(['src/*.js'])
+        .pipe(wrap({
+            namespace: 'UISwitch'
+        }))
+        .pipe(gulp.dest('dist/'));
 });
